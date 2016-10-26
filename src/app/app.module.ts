afbc522f56919896125241bfed75e7c3da441a81
boyboi86 <boyboi86@gmail.com>
2016-10-26 21:53:40 +0800

    link up with routes module and import routing class

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { BrowserModule  } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
-import { AppComponent } from './app.component';
+import { AppComponent }   from './app.component';
+import { HeaderComponent } from "./shared/header.component";
+import { SigninComponent } from "./unprotected/signin.component";
+import { SignupComponent } from "./unprotected/signup.component";
+import { ProtectedComponent } from "./protected/protected.component";
+import { AuthenticateRoutingModule } from './app-routing.module';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HeaderComponent,
+        SigninComponent,
+        SignupComponent,
+        ProtectedComponent
+    ],
+    imports: [BrowserModule, HttpModule, ReactiveFormsModule, AuthenticateRoutingModule],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
