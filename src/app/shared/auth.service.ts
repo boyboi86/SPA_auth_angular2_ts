9d80c0fafe0f839207a382535210e5d555c0c7aa
boyboi86 <boyboi86@gmail.com>
2016-10-26 22:49:51 +0800

    created new service with firebase auth

---
+++ src/app/shared/auth.service.ts
@@ -0,0 +1,13 @@
+import { User } from './user.interface';
+/*Declare used only in situation where script/cdn used but not SDK to tell TS it's a known obj*/
+declare const firebase: any;
+
+/*Use firebase for backend authentication*/
+export class AuthService {
+  signupUser(user: User){
+    firebase.auth().createUserWithEmailAndPassword(user.email, user.password)
+                    .catch(function(err){
+                      console.log(err);
+                    })
+  }
+}
