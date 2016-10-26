a0e5d0189ddaaaeba4dd3dd7bba6deca2b9ad77e
boyboi86 <boyboi86@gmail.com>
2016-10-26 21:51:39 +0800

    created routes && export Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SignupComponent } from './unprotected/signup.component';
+import { SigninComponent } from './unprotected/signin.component';
+import { ProtectedComponent } from './protected/protected.component';
 
-const routes: Routes = [];
+const routes: Routes  = [
+  { path:'', redirectTo:'/signup', pathMatch:'full'},
+  { path:'signup', component: SignupComponent},
+  { path:'signin', component: SigninComponent},
+  { path:'protected', component: ProtectedComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
